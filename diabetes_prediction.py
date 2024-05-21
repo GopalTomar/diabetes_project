bb2189e56943dea975bcf6bf0594e81e35549e8d
Gopal Tomar <116877746+GopalTomar@users.noreply.github.com>
2024-05-21 23:10:19 +0530

    diabetes_prediction.py

--- diabetes_prediction.py
+++ diabetes_prediction.py
@@ -39,7 +39,7 @@ Y = diabetes_dataset['Outcome']
 # X_train, X_test, Y_train, Y_test = train_test_split(standardized_data, Y, test_size=0.2, stratify=Y, random_state=2)
 
 # Train the SVM classifier
-# classifier = svm.SVC(kernel='linear')
+classifier = svm.SVC(kernel='linear')
 classifier.fit(X_train, Y_train)
 
 # Training data accuracy
