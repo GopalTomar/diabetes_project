50a39e2cb0e1017941d31379b8a255425bbf6804
Gopal Tomar <116877746+GopalTomar@users.noreply.github.com>
2024-05-21 22:55:07 +0530

    diabetes_prediction.py

--- diabetes_prediction.py
+++ diabetes_prediction.py
@@ -4,7 +4,7 @@ import pandas as pd
 # from sklearn.preprocessing import StandardScaler
 # from sklearn.model_selection import train_test_split
 # from sklearn import svm
-from sklearn.metrics import accuracy_score
+# from sklearn.metrics import accuracy_score
 
 # Load the diabetes dataset
 diabetes_dataset = pd.read_csv('diabetes.csv')
