9f015b5aa7869a1268203f800fc8642cd63d8fa6
Gopal Tomar <116877746+GopalTomar@users.noreply.github.com>
2024-05-21 22:54:13 +0530

    diabetes_prediction.py

--- diabetes_prediction.py
+++ diabetes_prediction.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 # from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
+# from sklearn.model_selection import train_test_split
 from sklearn import svm
 from sklearn.metrics import accuracy_score
 
